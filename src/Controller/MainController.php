cb7ffaa37250392c964fc1dcd1756036ef6dcbc2
VincentV <v@v.v>
2023-06-07 17:00:19 +0200

    Ajout d epage de profil

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class MainController extends AbstractController
 {
@@ -16,4 +17,18 @@ class MainController extends AbstractController
     {
         return $this->render('main/home.html.twig');
     }
+
+    /*
+     * Contrôleur de la page profil
+     *
+     * Accès réservé aux personnes connectées (ROLE_USER)
+     */
+
+    #[Route('/mon-profil/', name: 'mon-profil')]
+    #[IsGranted('ROLE_USER')]
+    public function profil(): Response
+    {
+        return $this->render('main/profil.html.twig');
+    }
+
 }
